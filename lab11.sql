80bf03eaa284d8fd025936e6be9dfa938b4e6a84
Zubryk <rom.zubr36@gmail.com>
2020-12-08 00:34:57 +0200

    lab11

---
+++ lab11.sql
@@ -0,0 +1,7 @@
+start transaction;
+insert into interviews value (null, '2020-05-04', '16:30:00.000', '17:00:00.000', 2, 3, 1, 1);
+insert into interviews value (null, '2020-05-06', '16:30:00.000', '17:00:00.000', 3, 2, 3, 0);
+insert into interviews value (null, '2020-07-08', '16:30:00.000', '17:00:00.000', 5, 1, 5, 1);
+insert into interviews value (null, '2020-09-09', '16:30:00.000', '17:00:00.000', 1, 5, 2, 0);
+insert into interviews value (null, '2020-09-11', '16:30:00.000', '17:00:00.000', 4, 4, 4, 1);
+commit;
